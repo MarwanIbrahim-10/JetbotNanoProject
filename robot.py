63607d13ee9899595af81e3b92ec4a3f22f8e7f8
Ahmad Elsaeed <ae2200@nyu.edu>
2023-11-12 16:05:18 +0400

    with openai integration

--- robot.py
+++ robot.py
@@ -2,6 +2,27 @@ from jetbot import Robot
 import time
 import cv2
 
+#Jupiter widgets to display images
+from IPython.display import display
+import ipywidgets.widgets as widgets
+from jetbot import bgr8_to_jpeg
+
+import base64
+import openai
+import os
+import requests
+import json
+
+openai_key = "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"
+
+openai.api_key = openai_key
+
+# client = OpenAI()
+
+image_widget = widgets.Image(format='jpeg')
+
+display(image_widget)
+
 daddyana = Robot()
 
 def move_forward(duration, spd):
@@ -47,6 +68,7 @@ def gstreamer_pipeline(capture_width=1280, capture_height=720, display_width=640
 # Start capturing from camera
 cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -54,24 +76,66 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
 
-    #the format of the frame variable is a numpy 3d array
-
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
     #process the image here!
+    # Convert the frame to JPEG format
+    _, buffer = cv2.imencode('.jpg', frame)
+
+    # Encode the image as base64
+    base64_image = base64.b64encode(buffer).decode()
+    
+    # Feed the image to the openai API
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"Bearer {openai_key}"
+#     }
+
+#     payload = {
+#         "model": "gpt-4-vision-preview",
+#         "messages": [
+#           {
+#             "role": "user",
+#             "content": [
+#               {
+#                 "type": "text",
+#                 "text": "What’s in the main thing in this image? Respond with 1 word only"
+#               },
+#               {
+#                 "type": "image_url",
+#                 "image_url": {
+#                   "url": f"data:image/jpeg;base64,{base64_image}"
+#                 }
+#               }
+#             ]
+#           }
+#         ],
+#         "max_tokens": 4
+#     }
+
+#     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    
+    # JSON data
+#     json_data = response.json()
+
+#     # Extracting the message content
+#     message_content = json_data['choices'][0]['message']['content']
+
+#     print(message_content)
+    
 
     #make decisions on how the robot should move here
-
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-
+    
+    image_widget.value = bgr8_to_jpeg(frame)
+    
+    
     # Break the loop with 'q'
     if cv2.waitKey(1) == ord('q'):
         break
-
+        
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
