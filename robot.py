65022ea954a4b1b994016e7a2dc7a4a8c2bc5fc7
Ahmad Elsaeed <ae2200@nyu.edu>
2023-11-26 13:05:49 +0400

    skeleton to be tested

--- robot.py
+++ robot.py
@@ -8,12 +8,6 @@ from IPython.display import display
 import ipywidgets.widgets as widgets
 from jetbot import bgr8_to_jpeg
 
-import base64
-import openai
-import os
-import requests
-import json
-
 image_widget = widgets.Image(format='jpeg')
 
 display(image_widget)
@@ -72,59 +66,116 @@ if not cap.isOpened():
 current_path = None  # The current path the robot is on
 last_intersection = None  # The last intersection type encountered
 
-def detect_intersection(thresholded_frame):
-    pass
+def follow_line(frame, display_width):
+    #convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #threshold the image to get the line in white
+    _, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+    #find the contours of the line
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        # Find the largest contour, assumed to be the line
+        largest_contour = max(contours, key=cv2.contourArea)
+        # Calculate the moments of the largest contour
+        M = cv2.moments(largest_contour)
+        if M["m00"] != 0:
+            # Calculate the x-coordinate of the centroid
+            cX = int(M["m10"] / M["m00"])
+            # Calculate the deviation from the center of the display width
+            deviation = cX - (display_width // 2)
+            return deviation
+    return None  # No line detected
 
-def decide_new_direction(current_path, intersection_type):  
-    pass
+#modify the detect_intersection function to return more detailed information
+def detect_intersection(hsv_frame):
+    #define range for green color
+    lower_green = np.array([40, 40, 40])
+    upper_green = np.array([70, 255, 255])
+    #threshold the HSV image to get only green colors
+    mask = cv2.inRange(hsv_frame, lower_green, upper_green)
+    #find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 100:  # Assuming markers have a significant area
+            # Additional logic to determine the type of intersection
+            # This can be based on the position or number of green markers
+            # For the sake of example, let's say you determine the direction based on the position
+            x, y, w, h = cv2.boundingRect(contour)
+            if x < hsv_frame.shape[1] // 2:
+                return 'left'
+            else:
+                return 'right'
+    return 'straight'  # Default to straight if no markers or specific conditions are not met
+
+
+def decide_new_direction(intersection_type):
+    # Implement logic based on the type of intersection
+    if intersection_type == 'left':
+        return 'C'  # Turn left
+    elif intersection_type == 'right':
+        return 'B'  # Turn right
+    elif intersection_type == 'straight':
+        return 'A'  # Continue straight
+    # Add more conditions as necessary, for example 'uturn'
+
 
 while True:
+    #read from camera
     ret, frame = cap.read()
 
-    # if frame is read correctly ret is True
+    #if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    #pass the by a filter to highlight the black and white parts of it, so we can see if the rectangular box exists
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    _, thresholded_frame = cv2.threshold(gray_frame, 100, 255, cv2.THRESH_BINARY_INV)  # Apply thresholding
+    print("Reading from camera...")
 
-    # Check for an intersection
-    intersection_type, intersection_center = detect_intersection(thresholded_frame)
+    #follow the line
+    #assumes display width is 640, will have to adjust that accordingly
+    deviation = follow_line(frame, 640)
+    if deviation is not None:
+        print(f"Line detected. Deviation: {deviation}")
+        #adjust robot's direction based on deviation
+        #proportional control of the robot
+        speed = 0.3  #base speed
+        k_p = 0.01  #proportional gain, adjust as necessary
+        turn_speed = k_p * deviation
+        daddyana.set_motors(speed + turn_speed, speed - turn_speed)
+        time.sleep(0.1)
+    else:
+        print("No line detected, stopping the robot.")
+        #no line detected, stop the robot or search for the line
+        daddyana.stop()
 
-    if intersection_type is not None:
-        # If we have detected an intersection, decide the new direction based on the rules
-        new_direction = decide_new_direction(current_path, intersection_type)
-
-        # Act on the new direction
+    #intersection detection logic
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    intersection_type = detect_intersection(hsv_frame)
+    if intersection_type:
+        print(f"Intersection detected: {intersection_type}")
+        new_direction = decide_new_direction(intersection_type)
+        print(f"New direction: {new_direction}")
+        # Implement the logic to handle the turn based on new_direction
         if new_direction == 'A':
-            # Assuming 'A' means keep going straight
-            move_forward(0.1, 0.3)
+            # Continue straight
+            move_forward(0.1, speed)  # Example duration and speed
         elif new_direction == 'B':
-            # Assuming 'B' means turn right
-            move_right(0.1, 0.3)
+            # Turn right
+            move_right(0.1, speed)  # Example duration and speed
         elif new_direction == 'C':
-            # Assuming 'C' means turn left
-            move_left(0.1, 0.3)
-        elif new_direction == 'D':
-            # Assuming 'D' means turn around or some other action
-            # Implement the necessary action
-            pass
+            # Turn left
+            move_left(0.1, speed)  # Example duration and speed
+        # Add more actions as necessary
+    else:
+        print("No intersection detected.")
 
-        # Update the current path and last intersection
-        current_path = new_direction
-        last_intersection = intersection_type
-
-    
-    
+    #update the image widget
     image_widget.value = bgr8_to_jpeg(frame)
-    
-    
-    # Break the loop with 'q'
+
+    #break the loop with 'q'
     if cv2.waitKey(1) == ord('q'):
         break
         
-# When everything done, release the capture
+#when everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
